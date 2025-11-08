9b436d215421262eb9dbb62bd2d08cc165e91ce3
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2025-11-08 19:14:07 +0300

    Initial commit: image compression project

---
+++ DQNAgent.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class QModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+        self.path = "./exit_model/model.pth"
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+
+    def save(self):
+        os.makedirs(os.path.dirname(self.path), exist_ok=True)
+        torch.save(self.state_dict(), self.path)
+
+
+class QTrainer:
+    def __init__(self, lr, gamma, model):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        pred = self.model(state)
+        target = pred.clone()
+
+        for i in range(len(done)):
+            Q_new = reward[i]
+            if not done[i]:
+                Q_new = reward[i] + self.gamma * torch.max(self.model(next_state[i]))
+            target[i][torch.argmax(action[i]).item()] = Q_new
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(pred, target)
+        loss.backward()
+        self.optimizer.step()
